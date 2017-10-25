47c51ec10b4ad9acaf305c922e3b5f6f96d16cd8
StefanU <stefanpu13@gmail.com>
2017-10-25 14:29:55 +0300

    Add code

---
+++ SwaggerUI-Server/Db.fs
@@ -0,0 +1,39 @@
+﻿namespace DAL
+module Db =
+    open System.IO
+    open System
+
+    type User = { Username: string; PasswordHash :string }
+
+    type QueryResult<'a> = Result of 'a| NotFound
+    type Registration  = UserRegistered of string | UsernameAlreadyExists
+
+    let private dbLoc =  __SOURCE_DIRECTORY__ + @"\Db.csv"
+
+    let private users () = 
+        File.ReadAllLines(dbLoc)
+        |> Seq.skip 1
+        |> Seq.map (fun (line: string) -> 
+            let userData = line.Split([|','|], StringSplitOptions.RemoveEmptyEntries) 
+            { Username = userData.[0].Trim(); PasswordHash = userData.[1].Trim()}
+        )
+        |> Array.ofSeq
+        
+    let addUser (username, password, createdBy) =         
+        let newUser = username + ", " + password
+        File.AppendAllLines(dbLoc, [newUser])
+
+    let loginUser username password = 
+        let byNameAndPassword u = 
+            u.Username = username && u.PasswordHash = password
+
+        match Seq.tryFind byNameAndPassword (users()) with
+        | Some u -> Result u
+        | _ -> NotFound
+
+    let registerUser (username, password, createdBy) = 
+        match users() |> Seq.exists (fun u -> u.Username = username) with
+        | true -> UsernameAlreadyExists
+        | false -> 
+            addUser (username, password, createdBy) |> ignore
+            UserRegistered username
---
+++ SwaggerUI-Server/Program.fs
@@ -0,0 +1,53 @@
+﻿open Suave
+open Suave.Filters
+open Suave.Operators
+open Suave.Successful
+open Suave.RequestErrors
+open Suave.Writers
+open Suave.Web
+open System
+
+open DAL.Db
+
+let setCORSHeaders =
+    setHeader "Access-Control-Allow-Origin" "*"     
+    >=> setHeader "Access-Control-Allow-Headers" "content-type"     
+
+let setCORS context = 
+    context |> (
+        setCORSHeaders
+        >=> OK "CORS approved" )
+
+let allowCors : WebPart =
+    choose [
+        OPTIONS >=> setCORS
+        //GET >=> setCORS            
+    ]
+
+[<EntryPoint>]
+let main argv = 
+    let webPart = 
+        choose (
+            [
+                //allowCors
+                GET >=> pathScan "/webapi/2.0/ApiDocumentationUsers/Login/%s/%s" (fun (username, pass) -> 
+                    match loginUser username pass with
+                    | Result u -> OK (u.Username)
+                    | NotFound -> BAD_REQUEST ("Could not login.")
+                )
+            
+                POST >=> pathScan "/webapi/2.0/ApiDocumentationUsers/Register/%s/%s/%s" (fun data -> 
+                    match registerUser data with
+                    | UserRegistered username -> OK (username)
+                    | UsernameAlreadyExists -> BAD_REQUEST ("User already exists.")
+                ) >=> setCORSHeaders
+            ] |> List.map ((>=>) setCORSHeaders)
+        )
+
+    startWebServer 
+        ({defaultConfig with 
+            bindings = 
+                [ HttpBinding.createSimple HTTP "127.0.0.1" 8081]}
+        ) 
+        webPart
+    0 // return an integer exit code
